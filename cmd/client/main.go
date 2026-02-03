95636dd26058378225190b01dae74f665c1dbd6d
Brian Cepon <cepon.b@gmail.com>
2026-02-03 16:19:22 -0300

    feat: get bytes flowing

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+)
 
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	conn, err := net.Dial("tcp", ":1337")
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	message := "Hello World!\n"
+	messageBytes := []byte(message)
+
+	n, err := conn.Write(messageBytes)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info(fmt.Sprintf("Wrote %d bytes", n))
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"net"
+	"os"
+)
 
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	listener, err := net.Listen("tcp", ":1337")
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := listener.Accept()
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info(string(buf[:n]))
 }
