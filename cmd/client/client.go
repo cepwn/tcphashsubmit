1c1ab581682b9ca0e22ea3ced9e6bfb7266e369b
Brian Cepon <cepon.b@gmail.com>
2026-02-04 21:32:46 -0300

    refactor: handlers and other functions into client.go

---
+++ cmd/client/client.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/cepwn/tcphashsubmit/internal/models"
+	"github.com/cepwn/tcphashsubmit/internal/util"
+)
+
+func (app *application) listenForMessages() {
+	for {
+		messageEnvelope := &models.MessageEnvelope{}
+		err := app.decoder.Decode(messageEnvelope)
+		if err != nil {
+			app.logger.Error("unknown message type received", "message", messageEnvelope)
+			return
+		}
+		app.logger.Info("received message envelope:", "id", messageEnvelope.ID)
+
+		if messageEnvelope.ID == nil {
+			app.handleTaskAssignmentRequest(messageEnvelope)
+		} else {
+			app.handleResponse(messageEnvelope)
+		}
+	}
+}
+
+func (app *application) submitResults() {
+	ticker := time.NewTicker(2 * time.Second) // Between 1/sec and 1/min
+	defer ticker.Stop()
+
+	for range ticker.C {
+		jobID, serverNonce := app.clientState.getState()
+
+		if jobID == 0 {
+			continue
+		}
+
+		clientNonce, err := util.GenerateNonce()
+		if err != nil {
+			app.logger.Error("failed to generate nonce", "error", err)
+			continue
+		}
+		hash := util.ComputeSHA256(serverNonce + clientNonce)
+		err = app.sendResultSubmissionRequest(jobID, clientNonce, hash)
+		if err != nil {
+			app.logger.Error("error sending result submission request:", "error", err)
+			return
+		}
+
+	}
+}
+
+func (app *application) handleResponse(messageEnvelope *models.MessageEnvelope) {
+	if messageEnvelope.Result != nil && *messageEnvelope.Result {
+		app.logger.Info("request succeeded", "id", messageEnvelope.ID)
+	} else {
+		errMsg := "unknown error"
+		if messageEnvelope.Error != nil {
+			errMsg = *messageEnvelope.Error
+		}
+		app.logger.Error("request failed", "id", messageEnvelope.ID, "error", errMsg)
+	}
+}
+
+func (app *application) handleTaskAssignmentRequest(messageEnvelope *models.MessageEnvelope) {
+	params := &models.TaskAssignmentRequestParams{}
+	err := json.Unmarshal(messageEnvelope.Params, params)
+	if err != nil {
+		app.logger.Error(err.Error())
+		return
+	}
+	app.clientState.setState(params.JobID, params.ServerNonce)
+	app.logger.Info("received task assignment request", "job_id", params.JobID)
+}
+
+func (app *application) sendAuthorizationRequest() {
+	requestID := app.clientState.getNextRequestID()
+	authorizationRequest := &models.AuthenticationRequest{
+		BaseRequest: models.BaseRequest{
+			BasePayload: models.BasePayload{
+				ID: &requestID,
+			},
+			Method: "authorize",
+		},
+		Params: models.AuthenticationRequestParams{
+			Username: "test",
+		},
+	}
+	err := app.encoder.Encode(authorizationRequest)
+	if err != nil {
+		app.logger.Error(err.Error())
+	}
+	return
+}
+
+func (app *application) sendResultSubmissionRequest(jobID int, clientNonce string, hash string) error {
+	requestID := app.clientState.getNextRequestID()
+	resultSubmissionRequest := &models.ResultSubmissionRequest{
+		BaseRequest: models.BaseRequest{
+			BasePayload: models.BasePayload{
+				ID: &requestID,
+			},
+			Method: "submit",
+		},
+		Params: models.ResultSubmissionRequestParams{
+			JobID:       jobID,
+			ClientNonce: clientNonce,
+			Result:      hash,
+		},
+	}
+	err := app.encoder.Encode(resultSubmissionRequest)
+	if err != nil {
+		return err
+	}
+	app.logger.Info("sent result submission request")
+	return nil
+}
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,9 +5,14 @@ import (
 	"log/slog"
 	"net"
 	"os"
-
-	"github.com/cepwn/tcphashsubmit/internal/models"
 )
+
+type application struct {
+	logger      *slog.Logger
+	clientState *clientState
+	encoder     *json.Encoder
+	decoder     *json.Decoder
+}
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
@@ -23,91 +28,20 @@ func main() {
 
 	defer conn.Close()
 
+	clientState := newClientState()
 	decoder := json.NewDecoder(conn)
 	encoder := json.NewEncoder(conn)
 
-	sendAuthorizationRequest(encoder, logger)
-	listenForMessages(decoder, logger)
-}
+	app := &application{
+		logger:      logger,
+		clientState: clientState,
+		decoder:     decoder,
+		encoder:     encoder,
+	}
 
-func listenForMessages(decoder *json.Decoder, logger *slog.Logger) {
-	for {
-		messageEnvelope := &models.MessageEnvelope{}
-		err := decoder.Decode(messageEnvelope)
-		if err != nil {
-			logger.Error("unknown message type received", "message", messageEnvelope)
-			return
-		}
-		logger.Info("received message envelope:", "id", messageEnvelope.ID)
+	app.sendAuthorizationRequest()
+	go app.listenForMessages()
+	go app.submitResults()
 
-		if messageEnvelope.ID == nil {
-			handleTaskAssignmentRequest(messageEnvelope, logger)
-		} else {
-			handleResponse(messageEnvelope, logger)
-		}
-	}
-}
-
-func handleResponse(messageEnvelope *models.MessageEnvelope, logger *slog.Logger) {
-	if messageEnvelope.Result != nil && *messageEnvelope.Result {
-		logger.Info("request succeeded", "id", messageEnvelope.ID)
-	} else {
-		errMsg := "unknown error"
-		if messageEnvelope.Error != nil {
-			errMsg = *messageEnvelope.Error
-		}
-		logger.Error("request failed", "id", messageEnvelope.ID, "error", errMsg)
-	}
-}
-
-func handleTaskAssignmentRequest(messageEnvelope *models.MessageEnvelope, logger *slog.Logger) {
-	params := &models.TaskAssignmentRequestParams{}
-	err := json.Unmarshal(messageEnvelope.Params, params)
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-	logger.Info("received task assignment request", "job_id", params.JobID)
-}
-
-func sendAuthorizationRequest(encoder *json.Encoder, logger *slog.Logger) {
-	authRequestID := 1
-	authorizationRequest := &models.AuthenticationRequest{
-		BaseRequest: models.BaseRequest{
-			BasePayload: models.BasePayload{
-				ID: &authRequestID,
-			},
-			Method: "authorize",
-		},
-		Params: models.AuthenticationRequestParams{
-			Username: "test",
-		},
-	}
-	err := encoder.Encode(authorizationRequest)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return
-}
-
-func sendResultSubmissionRequest(encoder *json.Encoder, logger *slog.Logger) {
-	resultSubmissionRequestID := 2
-	resultSubmissionRequest := &models.ResultSubmissionRequest{
-		BaseRequest: models.BaseRequest{
-			BasePayload: models.BasePayload{
-				ID: &resultSubmissionRequestID,
-			},
-			Method: "submit",
-		},
-		Params: models.ResultSubmissionRequestParams{
-			JobID:       1,
-			ClientNonce: "1234567890",
-		},
-	}
-	err := encoder.Encode(resultSubmissionRequest)
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-	logger.Info("sent result submission request")
+	select {}
 }
